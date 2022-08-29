ab731540a1ae243bdf6bb1c00cc3bd9445588ee7
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-08-29 14:26:47 +0200

    update

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
 from os import listdir
 from os.path import isfile, join
+import re
 import json
 
+base_path = "icons/hero-icons"
+catagories = listdir(base_path)
+
 data = []
-
-paths = [
-    "brands", 
-    "solid",
-    "regular"
-]
-
-github = 'https://raw.githubusercontent.com/moesmoesie/fontawesome-loader/master'
-
-for path in paths:
-    localPath = "fontawesome-6.1.2-web/svgs/" + path
-    onlyfiles = [f for f in listdir(localPath) if isfile(join(localPath, f))]
-    for file in onlyfiles:
+for catagory in catagories:
+    path = base_path + "/" + catagory
+    files = [f for f in listdir(path) if isfile(join(path, f))]
+    for file in files:
+        svgPath = path + "/" + file
         svg=""
-        with open(localPath+ "/" + file) as f:
+        with open(svgPath) as f:
             svg = f.read()
-        print(svg)
-        name = path + "/" + file.split(".")[0]
+        svg = re.sub(r'(stroke=\"((?!none).*?)\")',' stroke="currentColor"', svg)
+        svg = re.sub(r'(fill=\"((?!none).*?)\")',' fill="currentColor"', svg)
+        svg = re.sub(r' width=\"[^\"]*\"', '', svg)
+        svg = re.sub(r' height=\"[^\"]*\"', '', svg)
+
+        name = "hero-icons" + "/" + catagory + "/" + file
         value = svg
+
         data.append({
             'name' : name,
             'value': value
         })
 
-with open("fontawesome.json", "w") as outfile:
+with open("icons.json", "w") as outfile:
     json.dump(data, outfile)
