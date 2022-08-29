b89f68d8f8c92b51184e8e5754ff067a25f0a80f
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-08-29 08:41:47 +0200

    Change loading style

--- main.py
+++ main.py
@@ -1,40 +1,24 @@
-from asyncio import constants
-import urllib.parse
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+from os import listdir
+from os.path import isfile, join
 import json
 
-driver = webdriver.Safari()
+data = {}
 
-url = "https://fontawesome.com/search?"
-full_data = []
+paths = [
+    "brands", 
+    "solid",
+    "regular"
+]
 
+github = 'https://raw.githubusercontent.com/moesmoesie/fontawesome-loader/master'
 
-for type in ["solid","brands"]:
-    currentPage = 1
-    while True:
-        params = {"m" : "free","p" : currentPage, "s":type}
-        fullUrl = url + urllib.parse.urlencode(params)
-        driver.get(fullUrl)
-        driver.implicitly_wait(5)
-        elements = driver.find_elements(By.CLASS_NAME, "wrap-icon")
-        currentPage += 1
+for path in paths:
+    localPath = "fontawesome-6.1.2-web/svgs/" + path
+    onlyfiles = [f for f in listdir(localPath) if isfile(join(localPath, f))]
+    for file in onlyfiles:
+        name = path + "/" + file.split(".")[0]
+        value = github + "/" + localPath + "/" + file
+        data[name] = value
 
-        if len(elements) == 0:
-            break;
-
-        print
-
-        for element in elements:
-            identifier = element.get_attribute("id")
-            values = identifier.split("-")
-            name = "-".join(values[1:-1])
-            data = {
-                "name" : name + " " + values[-1],
-                "value" : name + "@" + values[-1]
-            }
-            full_data.append(data)
-
-d = json.dumps(full_data)
-with open('fontawesome.json', 'w') as f:
-    json.dump(d, f)
+with open("fontawesome.json", "w") as outfile:
+    json.dump(data, outfile)
